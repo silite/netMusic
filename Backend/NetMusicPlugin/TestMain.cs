0a652801f896d3410aafe053f021a9a67b85f6e0
silite <lemnsilite@gmail.com>
2022-08-03 21:10:11 +0800

    chore: rename

---
+++ Backend/NetMusicPlugin/TestMain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using NetMusicPlugin;
+
+class TestMain
+{
+    static void Main(string[] args)
+    {
+
+        System.Timers.Timer timer = new System.Timers.Timer(1000);
+        timer.Elapsed += new System.Timers.ElapsedEventHandler(trigger);
+        timer.AutoReset = true; //设置是执行一次（false）还是一直执行(true)； 
+        timer.Enabled = true;
+        timer.Start();
+        while (true)
+        {
+        }
+    }
+
+    private static void trigger(object source, System.Timers.ElapsedEventArgs e)
+    {
+        ElementTool tools = ElementTool.Instance;
+        ArrayList resList = tools.getResList();
+        
+        ReadFile file = ReadFile.Instance;
+    }
+}
