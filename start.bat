69b9355c7b8f1cb58d8de7f730147570c8ba5c65
silite <lemnsilite@gmail.com>
2022-08-04 19:18:29 +0800

    feat: auto bat

---
+++ start.bat
@@ -0,0 +1,14 @@
+@echo off
+if "%1" == "h" goto begin
+mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
+:begin
+for /f "tokens=1-5" %%i in ('netstat -ano^|findstr ":12449"') do taskkill /pid %%m /f
+for /f "tokens=1-5" %%i in ('netstat -ano^|findstr ":10145"') do taskkill /pid %%m /f
+
+cd "Backend/NetMusicPlugin/bin/Debug/net48"
+start NetMusicPlugin.exe
+
+cd "../../../../../nodeMiddleware"
+node "index.js"
+
+pause
