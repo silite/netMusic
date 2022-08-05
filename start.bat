282190605785399c216d06021e236fdefde12539
silite <lemnsilite@gmail.com>
2022-08-05 14:30:20 +0800

    feat: start script done

--- start.bat
+++ start.bat
@@ -1,29 +1,16 @@
 @echo off
-@REM if "%1" == "h" goto begin
-@REM mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
-@REM :begin
+if "%1" == "h" goto begin
+mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
+:begin
 for /f "tokens=1-5" %%i in ('netstat -ano^|findstr ":12449"') do taskkill /pid %%m /f
 for /f "tokens=1-5" %%i in ('netstat -ano^|findstr ":10145"') do taskkill /pid %%m /f
 
-set binpath=%~dp0Backend\NetMusicPlugin\bin\Release\net48\NetMusicPlugin.exe
+cd /d %~dp0Backend\NetMusicPlugin\bin\Release\net48
+start NetMusicPlugin.exe
 
-for /f "delims=" %%i in ('sc query netMusicPlugin') do set querySc=%%i
+echo %~dp0
 
-sc stop netMusicPlugin
-
-echo %querySc%|find ":" >nul
-if errorlevel 1 (
-  sc create netMusicPlugin binPath= %binpath% displayname= netMusicPlugin start= demand
-) else (
-  sc config netMusicPlugin binPath= %binpath%
-)
-
-sc start netMusicPlugin
-
-@REM cd "Backend/NetMusicPlugin/bin/Debug/net48"
-@REM start NetMusicPlugin.exe
-
-cd "nodeMiddleware"
+cd %~dp0nodeMiddleware
 node "index.js"
 
 pause
