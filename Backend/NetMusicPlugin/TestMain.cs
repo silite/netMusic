e3337e2c34f72d3a757513197b09c8f055ff14c5
silite <lemnsilite@gmail.com>
2022-08-05 14:30:56 +0800

    fix: hide window

--- Backend/NetMusicPlugin/TestMain.cs
+++ Backend/NetMusicPlugin/TestMain.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
-using System.Diagnostics;
+using System.Runtime.InteropServices;
 using NetMusicPlugin;
 
 class TestMain
 {
+    [DllImport("user32.dll")]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    [DllImport("Kernel32")]
+    private static extern IntPtr GetConsoleWindow();
+
+    const int SW_HIDE=0;
+    const int SW_SHOW=5;
+    
     static void Main(string[] args)
     {
+        IntPtr hwnd;
+        hwnd=GetConsoleWindow();
+        ShowWindow(hwnd,SW_HIDE);
+        
         System.Timers.Timer timer = new System.Timers.Timer(1000);
         timer.Elapsed += new System.Timers.ElapsedEventHandler(trigger);
         timer.AutoReset = true; //设置是执行一次（false）还是一直执行(true)； 
