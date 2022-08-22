fa9c27b0b9fece78cd9cea6344ff2331cb0f7fd9
silite <lemnsilite@gmail.com>
2022-08-22 15:09:11 +0800

    chore: init msimg32 dll inject

---
+++ msimg32/dllmain.cpp
@@ -0,0 +1,20 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include<Windows.h>
+#include "EasyCEFHooks.h"
+
+using namespace std;
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, PVOID pvReserved)
+{
+	if (dwReason == DLL_PROCESS_ATTACH)
+	{
+		MessageBox(NULL, TEXT("test"), NULL, NULL);
+	}
+	else if (dwReason == DLL_PROCESS_DETACH)
+	{
+		
+	}
+	return TRUE;
+}
+
