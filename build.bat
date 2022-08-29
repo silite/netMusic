632739b8e2e72f459ae9546b5c481f5cbcdc0db1
silite <lemnsilite@gmail.com>
2022-08-30 01:55:02 +0800

    fix: build script

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@
   npm run pkg
 ) && (
   copy /Y %~dp0nodeMiddleware\dist-node\nodeMiddleware.exe %~dp0release
-  copy /Y %~dp0AppStart\AppStart\bin\Release\net48 %~dp0release\AppStart
-  copy /Y %~dp0Backend\NetMusicPlugin\bin\Release\net48 %~dp0release\Backend
+  @REM copy /Y %~dp0AppStart\AppStart\bin\Release\net48 %~dp0release\AppStart
+  @REM copy /Y %~dp0Backend\NetMusicPlugin\bin\Release\net48 %~dp0release\Backend
 )
--- release/stop.bat
+++ release/stop.bat
@@ -5,3 +5,4 @@ mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exi
 
 for /f "tokens=1-5" %%i in ('netstat -ano^|findstr ":12449"') do taskkill /pid %%m /f
 for /f "tokens=1-5" %%i in ('netstat -ano^|findstr ":10145"') do taskkill /pid %%m /f
+for /f "tokens=1-5" %%i in ('netstat -ano^|findstr ":13430"') do taskkill /pid %%m /f
