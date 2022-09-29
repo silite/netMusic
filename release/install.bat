eaf986584fafd6bbf23d7c177f72a3548d777bc3
silite <lemnsilite@gmail.com>
2022-09-29 13:21:47 +0800

    fix: install bat

--- release/install.bat
+++ release/install.bat
@@ -5,7 +5,7 @@ echo Pending...
 
 @REM ping localhost -n 2
 
-del "Z:\CloudMusic\msimg32.dll"
-copy .\msimg32.dll "\msimg32.dll">nul
+del "C:\Program Files (x86)\Netease\CloudMusic\msimg32.dll"
+copy .\msimg32.dll "C:\Program Files (x86)\Netease\CloudMusic\msimg32.dll">nul
 
 @REM Z:\CloudMusic\cloudmusic.exe
